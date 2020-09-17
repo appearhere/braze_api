5c53a37efb5aa2fc28033ec7217a74db5b90e57d
Janie Amero <janie.amero@gmail.com>
2020-09-17 14:53:15 +0100

    Add users/identify endpoint

---
+++ lib/braze_api/endpoints/users/identify.rb
@@ -0,0 +1,24 @@
+module BrazeAPI
+  module Endpoints
+    module Users
+      # Methods to call the users/identify endpoint from a client instance
+      module Identify
+        PATH = '/users/identify'.freeze
+        # The main method calling the endpoint.
+        # Called with an object containing the user alias object
+        # and the external id of the user being identified.
+        def identify(user_alias:, external_id:)
+          post(
+            PATH,
+            params: { aliases_to_identify: [
+              {
+                external_id: external_id,
+                user_alias: user_alias
+              }
+            ] }
+          )
+        end
+      end
+    end
+  end
+end
