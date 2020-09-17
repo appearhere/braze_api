aa52ab5834b2123b7b31d9e85e0e5e0fc526c9eb
Janie Amero <janie.amero@gmail.com>
2020-09-17 13:18:38 +0100

    Add users/alias endpoint

---
+++ lib/braze_api/endpoints/users/alias.rb
@@ -0,0 +1,21 @@
+module BrazeAPI
+  module Endpoints
+    module Users
+      # Methods to call the users/alias/new endpoint from a client instance
+      module Alias
+        PATH = '/users/alias/new'.freeze
+
+        # The main method calling the endpoint.
+        # Called with an alias name and an alias label.
+        def alias(alias_name:, alias_label:)
+          post(
+            PATH,
+            params: {
+              user_aliases: [{ alias_name: alias_name, alias_label: alias_label }]
+            }
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/braze_api/endpoints/users/alias_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe BrazeAPI::Endpoints::Users::Alias do
+  let(:api_key) { 'abcdefg' }
+  let(:app_id) { 'hijklmnop' }
+  let(:braze_url) { 'https://rest.fra-01.braze.eu' }
+  let(:alias_name) { 'hello@appearhere.co.uk' }
+  let(:alias_label) { 'email' }
+  let(:subject) { BrazeAPI::Client.new(api_key: api_key, app_id: app_id, braze_url: braze_url) }
+  before { allow(subject).to receive(:post).and_return('success') }
+
+  describe '.alias' do
+    it 'creates a user alias' do
+      expect(subject)
+        .to receive(:post)
+        .with(
+          '/users/alias/new',
+          params: { user_aliases: [{ alias_name: alias_name, alias_label: alias_label }] }
+        )
+
+      subject.alias(alias_name: alias_name, alias_label: alias_label)
+    end
+  end
+end
